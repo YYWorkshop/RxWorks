97eefa35962cb408cdf02a9e3c794c7104153257
Chris <chrissw.huang@optoma.com>
2018-12-21 15:00:51 +0800

    First commit.

---
+++ RxSimpleLogin/RxSimpleLogin/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RxSimpleLogin
+//
+//  Created by Chris Huang 黃信文 (奧圖碼) on 2018/12/21.
+//  Copyright © 2018 Chris. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
